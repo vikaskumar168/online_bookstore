18acfd8dee4de73b9e091f65d23b9bd02dfb0199
vikas <vikaskmr2411@gmail.com>
2024-04-11 13:32:32 +0530

    backend frontend connected

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
 import Spinner from "../Components/Spinner";
 
 const Home = () => {
@@ -26,12 +23,13 @@ const Home = () => {
 
   return (
     <>
-      <h1>home</h1>
       <div className="p-4">
         <div className="flex justify-between items-center">
           <h1 className="text-3xl my-8">Books List</h1>
           <Link to="/books/create">
-            <MdOutlineAddBox className="text-sky-800 text-4xl" />
+          <button className="text-2xl hover:text-yellow-500 text-blue-400">
+                        Create Book
+                      </button>
           </Link>
         </div>
         {loading ? (
@@ -40,7 +38,7 @@ const Home = () => {
           <table className="w-full border-separate border-spacing-2">
             <thead>
               <tr>
-                <th className="border border-slate-600 rounded-md">No</th>
+                <th className="border border-slate-600 rounded-md w-3">No.</th>
                 <th className="border border-slate-600 rounded-md">Title</th>
                 <th className="border border-slate-600 rounded-md max-md:hidden">
                   Author
@@ -53,35 +51,43 @@ const Home = () => {
                 </th>
               </tr>
             </thead>
-            {books.map((book, index) => (
-              <tr key={book._id} className="h-8">
-                <td className="border border-slate-700 rounded-md">
-                  {index + 1}
-                </td>
-                <td className="border border-slate-700 rounded-md">
-                  {book.title}
-                </td>
-                <td className="border border-slate-700 rounded-md max-md:hidden">
-                  {book.author}
-                </td>
-                <td className="border border-slate-700 rounded-md max-md:hidden">
-                  {book.PublishYear}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center">
-                  <div>
+            <tbody>
+              {books.map((book, index) => (
+                <tr key={book._id} className="h-8">
+                  <td className="border border-slate-700 rounded-md">
+                    {index + 1}.
+                  </td>
+                  <td className="border border-slate-700 rounded-md">
+                    {book.title}
+                  </td>
+                  <td className="border border-slate-700 rounded-md max-md:hidden">
+                    {book.author}
+                  </td>
+                  <td className="border border-slate-700 rounded-md max-md:hidden">
+                    {book.publishYear}
+                  </td>
+                  {/* <td className="border border-slate-700 rounded-md text-center"> */}
+                  <div className="flex px-6 justify-between">
                     <Link to={`/books/details/${book._id}`}>
-                      <BsInfoCircle className="text-2xl text-green-800" />
+                      <button className="text-2xl hover:text-yellow-500 text-blue-400">
+                        Book details
+                      </button>
                     </Link>
                     <Link to={`/books/edit/${book._id}`}>
-                      <AiOutlineEdit className="text-2xl text-yellow-600" />
+                      <button className="text-2xl text-blue-400 hover:text-green-500">
+                        Edit Books
+                      </button>
                     </Link>
                     <Link to={`/books/delete/${book._id}`}>
-                      <MdOutlineDelete className="text-2xl text-red-600" />
+                      <button className="text-2xl text-blue-400 hover:text-red-600">
+                        Delete Books
+                      </button>
                     </Link>
                   </div>
-                </td>
-              </tr>
-            ))}
+                  {/* </td> */}
+                </tr>
+              ))}
+            </tbody>
           </table>
         )}
       </div>
